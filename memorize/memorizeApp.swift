bd1b5e2a5d7078f12bd644dba1204461c3538b88
Mohamed Adawy <meladawi@2p.com.sa>
2025-03-05 11:10:51 +0200

    Initial Commit

---
+++ memorize/memorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorizeApp.swift
+//  memorize
+//
+//  Created by Mohamed Ghoneim on 05/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct memorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

6746745ce24b0eb3642044322c3b772f37491631
Mohamed Adawy <meladawi@2p.com.sa>
2026-02-09 09:30:04 +0200

    Done Animation lecture 8

---
+++ memorize/FlyingNumber.swift
@@ -0,0 +1,18 @@
+//
+//  FlyingNumber.swift
+//  memorize
+//
+//  Created by Mohamed Ghoneim on 12/01/2026.
+//
+
+import SwiftUI
+
+struct FlyingNumber: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FlyingNumber()
+}
